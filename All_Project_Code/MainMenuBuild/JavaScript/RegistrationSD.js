2b964dc4562c4351955f159083cae68c55408b12
LSoderquist <Luke.Soderquist@colorado.edu>
2021-03-25 18:18:53 -0600

    reg page additions

--- All_Project_Code/MainMenuBuild/JavaScript/RegistrationSD.js
+++ All_Project_Code/MainMenuBuild/JavaScript/RegistrationSD.js
@@ -1,17 +1,59 @@
 function registrationTabSwitch(id) {
+    document.getElementById(id).classList.add("active");
     if (id == "loginTab") {
-        document.getElementById(id).classList.add("active");
         document.getElementById("signUpTab").classList.remove("active");
-        document.getElementById("usernameLabel").innerHTML = "Username";
-        document.getElementById("passwordLabel").innerHTML = "Password";
-        document.getElementById("confirmPasswordGroup").style.display = "none";
-        document.getElementById("submit").innerHTML = "Login";
+        
+        var loginElements = document.getElementsByClassName("login");
+        var createAccountElements = document.getElementsByClassName("createAccount");
+        for (var i = 0; i < loginElements.length; i++) {
+            loginElements[i].style.display = "block";
+        }
+        for (var i = 0; i < createAccountElements.length; i++) {
+            createAccountElements[i].style.display = "none";
+            createAccountElements[i].value = '';
+        }
+
+        document.getElementById("submitLogin").style.display = "block";
+        document.getElementById("submitCreateAccount").style.display = "none";
     } else {
-        document.getElementById(id).classList.add("active");
         document.getElementById("loginTab").classList.remove("active");
-        document.getElementById("usernameLabel").innerHTML = "Create Username";
-        document.getElementById("passwordLabel").innerHTML = "Create Password";
-        document.getElementById("confirmPasswordGroup").style.display = "block";
-        document.getElementById("submit").innerHTML = "Create Account";
+
+        var loginElements = document.getElementsByClassName("login");
+        var createAccountElements = document.getElementsByClassName("createAccount");
+        for (var i = 0; i < loginElements.length; i++) {
+            loginElements[i].style.display = "none";
+            loginElements[i].value = '';
+        }
+        for (var i = 0; i < createAccountElements.length; i++) {
+            createAccountElements[i].style.display = "block";
+        }
+
+        document.getElementById("submitLogin").style.display = "none";
+        document.getElementById("submitCreateAccount").style.display = "block";
+    }
+}
+
+function loginHandler() {
+    var username = document.getElementById("inputUsername");
+    var password = document.getElementById("inputPassword");
+    var newUsername = document.getElementById("inputNewUsername");
+    var newPassword = document.getElementById("inputNewPassword");
+    var confirmPassword = document.getElementById("confirmPassword");
+    var submitLogin = document.getElementById("submitLogin");
+    var submitCreateAccount = document.getElementById("submitCreateAccount");
+
+    password.onkeyup = function() {
+        if (username.value.length > 0 && password.value.length >= 8) submitLogin.disabled = false;
+        else submitLogin.disabled = true;
+    }
+
+    newPassword.onkeyup = function() {
+        if (newUsername.value.length > 0 && newPassword.value.length >= 8 && newPassword.value == confirmPassword.value) submitCreateAccount.disabled = false;
+        else submitCreateAccount.disabled = true;
+    }
+
+    confirmPassword.onkeyup = function() {
+        if (newUsername.value.length > 0 && newPassword.value.length >= 8 && newPassword.value == confirmPassword.value) submitCreateAccount.disabled = false;
+        else submitCreateAccount.disabled = true;
     }
 }
--- All_Project_Code/MainMenuBuild/server.js
+++ All_Project_Code/MainMenuBuild/server.js
@@ -22,8 +22,92 @@ app.use(express.static(__dirname + '/'));
 // registration page
 app.get('/', function(req, res) {
 	res.render('registration',{
-		my_title:"Registration Page"
+		my_title:"Registration Page",
+		message: '',
+		success: false,
+		loginTab: true
 	});
+});
+
+app.post('/', function(req, res) {
+	var username = req.body.username;
+	var password = req.body.password;
+
+	var createUsername = req.body.createUsername;
+	var createPassword = req.body.createPassword;
+
+	var users = "select * from users;";
+	var user = "select * from users where username='" + username + "';";
+	var insert_statement = "insert into users (username, pw) values ('" + username + "', '" + password + "');"; 
+	var username_available = "select count(*) from users where username='" + createUsername + "';";
+	var login_check = "select username, pw from users where username='" + username + "';"
+
+	if (username) {
+		db.task('get-everything', task => {
+			return task.batch([
+				task.any(login_check)
+			]);
+		})
+		.then(info => {
+			if (info[0][0] && info[0][0].pw == password) {
+				res.render('registration',{
+					my_title: "Registration Page",
+					message: "Login successful!",
+					success: true,
+					loginTab: true
+				})
+			} else {
+				res.render('registration',{
+					my_title: "Registration Page",
+					message: "Incorrect username or password",
+					success: false,
+					loginTab: true
+				})
+			}
+		})
+		.catch(err => {
+			console.log('error', err);
+			res.render('registration', {
+				my_title: 'Registration Page',
+				message: '',
+				success: false,
+				loginTab: true
+			})
+		});
+	} else {
+		db.task('get-everything', task => {
+			return task.batch([
+				task.any(username_available)
+			]);
+		})
+		.then(info => {
+			console.log(info[0][0].count);
+			if (info[0][0].count == 0) {
+				res.render('registration',{
+					my_title: "Registration Page",
+					message: "Account created successfully!",
+					success: true,
+					loginTab: false
+				})
+			} else {
+				res.render('registration',{
+					my_title: "Registration Page",
+					message: "This username has been taken",
+					success: false,
+					loginTab: false
+				})
+			}
+		})
+		.catch(err => {
+			console.log('error', err);
+			res.render('registration', {
+				my_title: 'Registration Page',
+				message: '',
+				success: false,
+				loginTab: false
+			})
+		});
+	}
 });
 
 app.get('/home', function(req, res) {
